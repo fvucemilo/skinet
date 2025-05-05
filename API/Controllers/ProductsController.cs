e2443479a1cfe063a5141d66a7f7788bfd1c7a98
fvgrgic <fvgrgic@FVGRGIC>
2025-05-05 11:47:17 +0200

    Add project files.

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,88 @@
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContex contex;
+
+        public ProductsController(StoreContex contex)
+        {
+            this.contex = contex;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        { 
+            return await contex.Products.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await contex.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return product;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> CreateProduct(Product product)
+        {
+            contex.Products.Add(product);
+            await contex.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetProduct), new { id = product.Id }, product);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> UpdateProduct(int id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest("Cannot update this product");
+            }
+            contex.Entry(product).State = EntityState.Modified;
+            try
+            {
+                await contex.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var product = await contex.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            contex.Products.Remove(product);
+            await contex.SaveChangesAsync();
+            return NoContent();
+        }
+
+        private bool ProductExists(int id)
+        {
+            return contex.Products.Any(x => x.Id == id);
+        }
+    }
+}
